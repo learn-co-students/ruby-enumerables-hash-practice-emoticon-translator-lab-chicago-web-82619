05cef9323bbeaa6425ee9ac0d4a758c27ad9161c
Aaron Blum <blumaa@gmail.com>
2019-08-02 14:40:08 +0000

    Done.

--- lib/translator.rb
+++ lib/translator.rb
@@ -1,6 +1,12 @@
 # require modules here
+require "pry"
+require "yaml"
 
-def load_library
+# emoticons = YAML.load_file('emoticons.yml')
+
+def load_library(emoticons)
+  faces = YAML.load_file('emoticons.yml')
+  binding.pry
   # code goes here
 end
 
