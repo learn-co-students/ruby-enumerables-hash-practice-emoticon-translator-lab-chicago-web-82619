9ffe819b22623ad8c9fa4b4991c869be4ac43251
Aaron Blum <blumaa@gmail.com>
2019-08-02 20:47:57 +0000

    Done.

--- lib/translator.rb
+++ lib/translator.rb
@@ -5,23 +5,30 @@ require "yaml"
 
 def load_library(emoticons)
     loaded_emoticons = YAML.load_file(emoticons)
-    sorted = {}
+    sorted = {
+      get_meaning: {},
+      get_emoticon: {}
+    }
     loaded_emoticons.each do |word, symbols|
-      # symbols.each do |a_e|
-        sorted = {
-          get_meaning: {},
-          get_emoticon: {
-          }
-        }
-        sorted[:get_emoticon] = symbols[1]
+         sorted[:get_emoticon][symbols[0]] = symbols[1]
+         sorted[:get_meaning][symbols[1]] = word
 # binding.pry
-      # end
+
     end
     sorted
 end
 
-def get_japanese_emoticon
-  # code goes here
+def get_japanese_emoticon(emoticons, emoticon)
+    new_lib = load_library(emoticons)
+    new_lib.each do |name, props|
+      props.each do |emote|
+        # binding.pry
+        if emote.include?(emoticon)
+           trans_emote = emote[1]
+        end
+      end
+    end
+    trans_emote
 end
 
 def get_english_meaning
